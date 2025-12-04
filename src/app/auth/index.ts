198c0a639de55eece927d45d85314adfb6b16be2
Carlos Martorell <cmartorellotal@gmail.com>
2025-12-04 12:03:31 +0100

    refactor(auth): Implement shared 'barrel' for auth components

---
+++ src/app/auth/index.ts
@@ -0,0 +1,9 @@
+export * from './guards/auth-guard';
+export * from './guards/guest-guard';
+export * from './services/auth';
+export * from './services/auth-interceptor';
+export * from './model/user';
+export * from './pages/login/login';
+export * from './pages/register/register';
+
+
