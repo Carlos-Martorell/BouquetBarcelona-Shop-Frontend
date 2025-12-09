f510ed21ee13ded46cf10fecc4e9de884712bf2c
Carlos Martorell <cmartorellotal@gmail.com>
2025-12-09 07:46:50 +0100

    refactor(cart): Use computed signal for cart count in Navbar

--- src/app/shared/ui/navbar/navbar.ts
+++ src/app/shared/ui/navbar/navbar.ts
@@ -1,11 +1,12 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { RouterLink, RouterLinkActive, Router } from '@angular/router';
 import { CartService } from '@cart';
 import { AuthService } from '@auth';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CurrencyPipe],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
@@ -14,11 +15,14 @@ export class Navbar {
   cartService = inject(CartService)
   router = inject(Router);
 
+  totalItemsCount = computed(() => this.cartService.totalItemsCount())
+  cartTotal = computed(() =>this.cartService.cartTotal())
+
   onLogout() {
     this.authService.logout();
     this.router.navigate(['/']);
   }
 
 
-  
+
 }
