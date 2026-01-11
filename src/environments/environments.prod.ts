8cb94209960392bae52420975e4e67be4f453404
Carlos Martorell <cmartorellotal@gmail.com>
2026-01-11 11:34:30 +0100

    security: stop tracking environment files in this branch

--- src/environments/environments.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-   apiUrl: 'https://bouquetbarcelona-backend.onrender.com',
-
-  mapboxKey: `pk.eyJ1IjoibWFydG83IiwiYSI6ImNtaWJ4ODJ0aTByMnYyaXM2MnQxOWttamUifQ.JcneSp7-ZnRFcaQPziY2Cg`,
-};
